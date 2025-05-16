23a8bda80cf78844fb5a8cff3a6a0482630d9724
Qwayb <girdden@gmail.com>
2025-05-17 01:58:42 +0700

    compatibility with old version 3

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -2,17 +2,9 @@
 
 namespace Model;
 
-<<<<<<< HEAD
-=======
-use Illuminate\Database\Eloquent\Factories\HasFactory;
->>>>>>> abc4302c8843b920c617753fae1b45058f0d1e7a
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-<<<<<<< HEAD
-=======
-    use HasFactory;
->>>>>>> abc4302c8843b920c617753fae1b45058f0d1e7a
     public $timestamps = false;
 }
--- app/Model/User.php
+++ app/Model/User.php
@@ -2,20 +2,11 @@
 
 namespace Model;
 
-<<<<<<< HEAD
-=======
-use Illuminate\Database\Eloquent\Factories\HasFactory;
->>>>>>> abc4302c8843b920c617753fae1b45058f0d1e7a
 use Illuminate\Database\Eloquent\Model;
 use Src\Auth\IdentityInterface;
 
 class User extends Model implements IdentityInterface
 {
-<<<<<<< HEAD
-=======
-    use HasFactory;
-
->>>>>>> abc4302c8843b920c617753fae1b45058f0d1e7a
     public $timestamps = false;
     protected $fillable = [
         'login',
@@ -31,27 +22,18 @@ class User extends Model implements IdentityInterface
         });
     }
 
-<<<<<<< HEAD
     // Выборка пользователя по первичному ключу
-=======
-    //Выборка пользователя по первичному ключу
->>>>>>> abc4302c8843b920c617753fae1b45058f0d1e7a
     public function findIdentity(int $id)
     {
         return self::where('id', $id)->first();
     }
 
-<<<<<<< HEAD
     // Возврат первичного ключа
-=======
-    //Возврат первичного ключа
->>>>>>> abc4302c8843b920c617753fae1b45058f0d1e7a
     public function getId(): int
     {
         return $this->id;
     }
 
-<<<<<<< HEAD
     // Возврат аутентифицированного пользователя
     public function attemptIdentity(array $credentials)
     {
@@ -59,12 +41,5 @@ class User extends Model implements IdentityInterface
             'login' => $credentials['login'],
             'password' => md5($credentials['password'])
         ])->first();
-=======
-    //Возврат аутентифицированного пользователя
-    public function attemptIdentity(array $credentials)
-    {
-        return self::where(['login' => $credentials['login'],
-            'password' => md5($credentials['password'])])->first();
->>>>>>> abc4302c8843b920c617753fae1b45058f0d1e7a
     }
 }
